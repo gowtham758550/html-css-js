f6f6dd86b7658408a8a8ef4999a69458aaa7cf63
gowtham758550 <gowtham758550@gmail.com>
2022-06-09 16:16:45 +0530

    categories added with spinner

--- jquery/main.js
+++ jquery/main.js
@@ -1,6 +1,26 @@
-$("#get-joke").click(function () {
-    $.get("https://api.chucknorris.io/jokes/random",function (data) {
-        // console.log(data);
+$(document).ready(function () {
+    $("#loader").show();
+    $("#joke").html("");
+    $.get(`https://api.chucknorris.io/jokes/random?query=${$("#joke-category option:selected").text()}`, function (data) {
+        $("#loader").hide();
         $("#joke").html(data.value);
-    });
+    })
+});
+
+$("#joke-category").on("change", function(){
+    $("#loader").show();
+    $("#joke").html("");
+    $.get(`https://api.chucknorris.io/jokes/random?query=${$("#joke-category option:selected").text()}`, function (data) {
+        $("#loader").hide();
+        $("#joke").html(data.value);
+    })
+});
+
+$("#get-joke").on("click", function(){
+    $("#loader").show();
+    $("#joke").html("");
+    $.get(`https://api.chucknorris.io/jokes/random?query=${$("#joke-category option:selected").text()}`,function (data) {
+        $("#loader").hide();
+        $("#joke").html(data.value);
+    }); 
 });
